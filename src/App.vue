fbd549454908ea908207aa0944aff5cbd25b5437
Peter <p.dyumin@dunice.net>
2023-08-28 17:31:24 +0300

    Move IO handlers out from App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,8 @@ import type { Socket } from 'socket.io-client';
 
 import type {
   AcknowledgementMessage,
-  ChunkData,
-  ChunkRequest,
-  DownloadedItem,
-  GenericFileData,
   ListedFile,
-  UpdateDeviceName,
-UpdateTotalDownloads,
 } from './types';
-import connection from './connection';
-import { convertArrayBufferChunksToBlob } from './utilities/binary';
 import DeviceNameModalComponent from './components/modals/DeviceNameModal.vue';
 import DownloadErrorModalComponent from './components/modals/DownloadErrorModal.vue';
 import EnterPasswordModalComponent from './components/modals/EnterPasswordModal.vue';
@@ -27,36 +19,30 @@ import FileDetailsModalComponent from './components/modals/FileDetailsModal.vue'
 import FooterComponent from './components/Footer.vue';
 import { getValue, setValue } from './utilities/storage';
 import HeaderComponent from './components/Header.vue';
+import ioState, { handleDisconnect } from './connection';
 import isMobile from './utilities/is-mobile';
 import PasswordModalComponent from './components/modals/PasswordModal.vue';
-import saveFileOnDisk from './utilities/save-file-on-disk';
 import SettingsModalComponent from './components/modals/SettingsModal.vue';
 import StyledSpinnerComponent from './components/elements/StyledSpinner.vue';
 import wakeLock from './utilities/wakelock';
 
 interface AppState {
-  connected: boolean;
   deviceName: string;
   downloadErrorMessage: string;
-  downloads: DownloadedItem[];
   enterPasswordModalFileId: string;
   fileDetailsFileId: string;
   isMobile: boolean;
-  listedFiles: ListedFile[];
   passwordModalFileId: string;
   showDeviceNameModal: boolean;
   showSettingsModal: boolean;
 }
 
 const state = reactive<AppState>({
-  connected: false,
   deviceName: '',
   downloadErrorMessage: '',
-  downloads: [],
   enterPasswordModalFileId: '',
   fileDetailsFileId: '',
   isMobile: false,
-  listedFiles: [],
   passwordModalFileId: '',
   showDeviceNameModal: false,
   showSettingsModal: false,
@@ -78,19 +64,19 @@ const closeModal = (modalName: string): void => {
 };
 
 const handleAddFile = (entry: ListedFile): void => {
-  state.listedFiles.push(entry);
+  ioState.listedFiles.push(entry);
 };
 
 const handleDeleteAllFiles = (): void => {
-  state.listedFiles = [];
+  ioState.listedFiles = [];
   state.showSettingsModal = false;
 };
 
 const handleDeleteFile = ({ fileId }: { fileId: string }): Socket => {
-  state.listedFiles = state.listedFiles.filter(
+  ioState.listedFiles = ioState.listedFiles.filter(
     (item: ListedFile): boolean => item.id !== fileId,
   );
-  return connection.io.emit(
+  return ioState.io.emit(
     EVENTS.deleteFile,
     { fileId },
   );
@@ -113,7 +99,7 @@ const handleDownloadFile = (
     grant?: string;
     ownerId: string;
   },
-): Socket => connection.io.emit(
+): Socket => ioState.io.emit(
   EVENTS.downloadFile,
   {
     fileId,
@@ -153,7 +139,7 @@ const handleFilePassword = (
     withPassword: boolean;
   },
 ): void => {
-  state.listedFiles.forEach((item: ListedFile): void => {
+  ioState.listedFiles.forEach((item: ListedFile): void => {
     if (item.id === fileId) {
       item.withPassword = withPassword;
     }
@@ -171,7 +157,7 @@ const handleShowPasswordModal = (fileId: string): void => {
 const handleStoreGrant = (
   { fileId = '', grant = '' }: { fileId: string, grant: string },
 ): void => {
-  state.listedFiles.forEach((item: ListedFile): void => {
+  ioState.listedFiles.forEach((item: ListedFile): void => {
     if (item.id === fileId) {
       item.grant = grant;
     }
@@ -183,261 +169,13 @@ const handleUpdateDeviceName = (value: string): void => {
   return handleDeviceName(value);
 };
 
-const ioHandlerChangePassword = (data: GenericFileData): void => {
-  const { fileId = '', ownerId = '' } = data;
-  state.listedFiles.forEach((item: ListedFile): void => {
-    if (item.id === fileId && item.ownerId === ownerId) {
-      item.withPassword = true;
-    }
-  });
-};
-
-const ioHandlerClientDisconnect = ({ id }: { id: string }): void => {
-  state.listedFiles = state.listedFiles.filter(
-    (entry: ListedFile): boolean => entry.ownerId !== id,
-  );
-};
-
-const ioHandlerDeleteAllFiles = ({ ownerId = '' }: { ownerId: string }): void => {
-  state.listedFiles = state.listedFiles.filter(
-    (item: ListedFile): boolean => item.ownerId !== ownerId,
-  );
-};
-
-const ioHandlerDeleteFile = ({ fileId = '' }: { fileId: string }): void => {
-  state.listedFiles = state.listedFiles.filter(
-    (item: ListedFile): boolean => item.id !== fileId,
-  );
-};
-
-const ioHandlerDownloadFile = (
-  data: { fileId: string; targetId: string },
-): null | Socket => {
-  const { fileId = '', targetId = '' } = data;
-  const [file] = state.listedFiles.filter(
-    (item: ListedFile): boolean => item.id === fileId && item.isOwner,
-  );
-  if (!file) {
-    return null;
-  }
-  return connection.io.emit(
-    EVENTS.uploadFileChunk,
-    {
-      chunk: file.chunks[0],
-      currentChunk: 1,
-      fileId,
-      fileName: file.fileName,
-      fileSize: file.fileSize,
-      ownerId: file.ownerId,
-      targetId,
-      totalChunks: file.chunks.length,
-      type: file.fileType,
-    },
-  );
-};
-
-const ioHandlerListFile = (data: ListedFile): void => {
-  state.listedFiles.push({
-    ...data,
-    downloadCompleted: false,
-    downloadPercent: 0,
-    grant: '',
-    isDownloading: false,
-    isOwner: false,
-  });
-};
-
-const ioHandlerRemoveFilePassword = (data: GenericFileData): void => {
-  const { fileId = '', ownerId = '' } = data;
-  state.listedFiles.forEach((item: ListedFile): void => {
-    if (item.id === fileId && item.ownerId === ownerId) {
-      item.withPassword = false;
-    }
-  });
-};
-
-const ioHandlerRequestFileChunk = (data: ChunkRequest): null | Socket => {
-  const {
-    chunkIndex,
-    fileId,
-    targetId,
-  } = data;
-  const [file = null] = state.listedFiles.filter(
-    (item: ListedFile): boolean => item.id === fileId,
-  );
-  if (!file) {
-    return null;
-  }
-  return connection.io.emit(
-    EVENTS.uploadFileChunk,
-    {
-      chunk: file.chunks[chunkIndex - 1],
-      currentChunk: chunkIndex,
-      fileId,
-      fileName: file.fileName,
-      fileSize: file.fileSize,
-      ownerId: file.ownerId,
-      targetId,
-      totalChunks: file.chunks.length,
-      type: file.fileType,
-    },
-  );
-};
-
-const ioHandlerRequestListedFiles = (data: ListedFile[]): void => {
-  if (Array.isArray(data) && data.length > 0) {
-    data.forEach((item: ListedFile): void => {
-      state.listedFiles.push({
-        ...item,
-        downloadCompleted: false,
-        downloadPercent: 0,
-        grant: '',
-        isDownloading: false,
-        isOwner: false,
-      });
-    });
-  }
-};
-
-const ioHandlerUpdateDeviceName = (data: UpdateDeviceName): void => {
-  const { newDeviceName = '', ownerId = '' } = data;
-  state.listedFiles.forEach((item: ListedFile): void => {
-    if (item.ownerId === ownerId) {
-      item.deviceName = newDeviceName;
-    }
-  });
-};
-
-const ioHandlerUpdateTotalDownloads = (data: UpdateTotalDownloads): void => {
-  const { fileId = '', totalDownloads = 0 } = data;
-  state.listedFiles.forEach((item: ListedFile): void => {
-    if (item.id === fileId) {
-      item.totalDownloads = totalDownloads;
-    }
-  });
-};
-
-const ioHandlerUploadFileChunk = (data: ChunkData): Socket | void => {
-  const {
-    chunk,
-    currentChunk,
-    fileId,
-    fileName,
-    fileSize,
-    ownerId,
-    targetId,
-    totalChunks,
-    type,
-  } = data;
-  if (currentChunk === 1 && totalChunks === 1) {
-    state.listedFiles.forEach((item: ListedFile): void => {
-      if (item.id === fileId) {
-        item.downloadCompleted = true;
-        item.downloadPercent = 100
-        item.isDownloading = false;
-      }
-    });
-    return saveFileOnDisk(
-      convertArrayBufferChunksToBlob([chunk], type),
-      fileName,
-    );
-  }
-  if (currentChunk === 1 && totalChunks > 1) {
-    state.listedFiles.forEach((item: ListedFile): void => {
-      if (item.id === fileId) {
-        item.downloadCompleted = false;
-        item.downloadPercent = Math.round(currentChunk / (totalChunks / 100));
-        item.isDownloading = true;
-      }
-    });
-    const newEntry: DownloadedItem = {
-      chunks: [chunk],
-      downloadCompleted: currentChunk === totalChunks,
-      fileId,
-      fileName,
-      fileSize,
-      ownerId,
-      totalChunks,
-      type,
-    };
-    state.downloads.push(newEntry);
-    return connection.io.emit(
-      EVENTS.requestFileChunk,
-      {
-        chunkIndex: currentChunk + 1,
-        fileId,
-        ownerId,
-        targetId,
-      },
-    );
-  }
-  if (currentChunk > 1 && currentChunk < totalChunks) {
-    state.downloads.forEach((item: DownloadedItem): void => {
-      if (item.fileId === fileId) {
-        item.chunks.push(chunk);
-      }
-    });
-    state.listedFiles.forEach((item: ListedFile): void => {
-      if (item.id === fileId) {
-        item.downloadPercent = Math.round(currentChunk / (totalChunks / 100));
-      }
-    });
-    return connection.io.emit(
-      EVENTS.requestFileChunk,
-      {
-        chunkIndex: currentChunk + 1,
-        fileId,
-        ownerId,
-        targetId,
-      },
-    );
-  }
-  if (currentChunk === totalChunks) {
-    const [downloadedFile] = state.downloads.filter(
-      (item: DownloadedItem): boolean => item.fileId === fileId,
-    );
-    state.listedFiles.forEach((item: ListedFile): void => {
-      if (item.id === fileId) {
-        item.downloadCompleted = true;
-        item.downloadPercent = 100;
-        item.isDownloading = false;
-      }
-    });
-    downloadedFile.chunks.push(chunk);
-    saveFileOnDisk(
-      convertArrayBufferChunksToBlob(downloadedFile.chunks, downloadedFile.type),
-      downloadedFile.fileName,
-    );
-    state.downloads = state.downloads.filter(
-      (item: DownloadedItem): boolean => item.fileId !== fileId,
-    );
-  }
-};
-
 const toggleModal = (modalName: string): void => {
   if (modalName === 'settings') {
     state.showSettingsModal = !state.showSettingsModal;
   }
 }
 
-onBeforeUnmount((): void => {
-  if (state.connected) {
-    const { io } = connection;
-    io.off(EVENTS.changePassword, ioHandlerChangePassword);
-    io.off(EVENTS.clientDisconnect, ioHandlerClientDisconnect);
-    io.off(EVENTS.deleteAllFiles, ioHandlerDeleteAllFiles);
-    io.off(EVENTS.deleteFile, ioHandlerDeleteFile);
-    io.off(EVENTS.downloadFile, ioHandlerDownloadFile);
-    io.off(EVENTS.listFile, ioHandlerListFile);
-    io.off(EVENTS.removePassword, ioHandlerRemoveFilePassword);
-    io.off(EVENTS.requestFileChunk, ioHandlerRequestFileChunk);
-    io.off(EVENTS.requestListedFiles, ioHandlerRequestListedFiles);
-    io.off(EVENTS.updateDeviceName, ioHandlerUpdateDeviceName);
-    io.off(EVENTS.updateTotalDownloads, ioHandlerUpdateTotalDownloads);
-    io.off(EVENTS.uploadFileChunk, ioHandlerUploadFileChunk);
-    io.emit(EVENTS.close);
-  }
-});
+onBeforeUnmount(handleDisconnect);
 
 onMounted((): void => {
   if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
@@ -460,27 +198,7 @@ onMounted((): void => {
     state.deviceName = deviceName;
   }
 
-  const { io } = connection;
-  io.open();
-  io.on(
-    EVENTS.connect,
-    (): void => {
-      io.emit(EVENTS.requestListedFiles);
-      io.on(EVENTS.changePassword, ioHandlerChangePassword);
-      io.on(EVENTS.clientDisconnect, ioHandlerClientDisconnect);
-      io.on(EVENTS.deleteAllFiles, ioHandlerDeleteAllFiles);
-      io.on(EVENTS.deleteFile, ioHandlerDeleteFile);
-      io.on(EVENTS.downloadFile, ioHandlerDownloadFile);
-      io.on(EVENTS.listFile, ioHandlerListFile);
-      io.on(EVENTS.removePassword, ioHandlerRemoveFilePassword);
-      io.on(EVENTS.requestFileChunk, ioHandlerRequestFileChunk);
-      io.on(EVENTS.requestListedFiles, ioHandlerRequestListedFiles);
-      io.on(EVENTS.updateDeviceName, ioHandlerUpdateDeviceName);
-      io.on(EVENTS.updateTotalDownloads, ioHandlerUpdateTotalDownloads);
-      io.on(EVENTS.uploadFileChunk, ioHandlerUploadFileChunk);
-      state.connected = true;
-    },
-  );
+  ioState.io.open();
 });
 </script>
 
@@ -491,7 +209,7 @@ onMounted((): void => {
       : 'h-100vh'}`"
   >
     <div
-      v-if="!state.connected"
+      v-if="!ioState.connected"
       class="f ai-center"
     >
       <div class="f d-col">
@@ -517,7 +235,7 @@ onMounted((): void => {
     <EnterPasswordModalComponent
       v-if="!!state.enterPasswordModalFileId"
       :is-mobile="state.isMobile"
-      :listed-file="state.listedFiles.filter(
+      :listed-file="ioState.listedFiles.filter(
         (item: ListedFile): boolean => item.id === state.enterPasswordModalFileId,
       )[0]"
       @close-modal="(): void => closeModal('enter-password')"
@@ -527,7 +245,7 @@ onMounted((): void => {
     <FileDetailsModalComponent
       v-if="!!state.fileDetailsFileId"
       :is-mobile="state.isMobile"
-      :listed-file="state.listedFiles.filter(
+      :listed-file="ioState.listedFiles.filter(
         (item: ListedFile): boolean => item.id === state.fileDetailsFileId,
       )[0]"
       @close-modal="(): void => closeModal('details')"
@@ -538,22 +256,22 @@ onMounted((): void => {
     <PasswordModalComponent
       v-if="!!state.passwordModalFileId"
       :is-mobile="state.isMobile"
-      :listed-file="state.listedFiles.filter(
+      :listed-file="ioState.listedFiles.filter(
         (item: ListedFile): boolean => item.id === state.passwordModalFileId,
       )[0]"
       @close-modal="(): void => closeModal('password')"
       @handle-file-password="handleFilePassword"
     />
     <div
-      v-if="state.connected"
+      v-if="ioState.connected"
       class="f d-col w-100"
     >
       <SettingsModalComponent
         v-if="state.showSettingsModal"
         :device-name="state.deviceName"
         :is-mobile="state.isMobile"
-        :shared-files="state.listedFiles.filter(
-          (item: ListedFile): boolean => item.ownerId === connection.io.id,
+        :shared-files="ioState.listedFiles.filter(
+          (item: ListedFile): boolean => item.ownerId === ioState.io.id,
         ).length"
         @close-modal="(): void => toggleModal('settings')"
         @delete-all-files="handleDeleteAllFiles"
@@ -562,16 +280,16 @@ onMounted((): void => {
       <HeaderComponent
         :device-name="state.deviceName"
         :is-mobile="state.isMobile"
-        :listed-files="state.listedFiles"
-        :owner-id="connection.io.id"
+        :listed-files="ioState.listedFiles"
+        :owner-id="ioState.io.id"
         @handle-add-file="handleAddFile"
         @toggle-settings-modal="(): void => toggleModal('settings')"
       />
       <FileListComponent
         :device-name="state.deviceName"
         :is-mobile="state.isMobile"
-        :listed-files="state.listedFiles"
-        :owner-id="connection.io.id"
+        :listed-files="ioState.listedFiles"
+        :owner-id="ioState.io.id"
         @handle-add-file="handleAddFile"
         @handle-delete-file="handleDeleteFile"
         @handle-download-file="handleDownloadFile"
@@ -579,7 +297,7 @@ onMounted((): void => {
         @handle-show-file-password-modal="handleShowEnterPasswordModal"
       />
       <FooterComponent
-        :backend-status="state.connected
+        :backend-status="ioState.connected
           ? 'connected'
           : 'inaccessible'"
         :is-mobile="state.isMobile"
