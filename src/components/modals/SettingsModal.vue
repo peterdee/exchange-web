9b729e08ffdec2cc8a3deaf8e55d663fb72d3475
Peter <dyumin.peter@yandex.com>
2025-04-27 19:36:46 +0300

    Fix modal content overflow, add public server button for Options modal

--- src/components/modals/SettingsModal.vue
+++ src/components/modals/SettingsModal.vue
@@ -24,9 +24,7 @@ const emit = defineEmits([
   'update-device-name',
 ]);
 
-const props = defineProps<{
-  sharedFiles: number;
-}>();
+const props = defineProps<{ sharedFiles: number }>();
 
 const state = reactive<ComponentState>({
   deviceName: store.deviceName,
@@ -89,6 +87,8 @@ const handleSubmitServerAddress = () => {
   }
   return window.location.replace(`${address}/?callback=${window.location.origin}`);
 };
+
+const handleUsePublicServer = () => window.location.replace(window.location.origin);
 </script>
 
 <template>
@@ -189,6 +189,17 @@ const handleSubmitServerAddress = () => {
         >
           Connect to the server
         </StyledButtonComponent>
+        <template v-if="store.isLocalServer">
+          <div class="f j-center mt-half">
+            <button
+              class="link ns"
+              type="button"
+              @click="handleUsePublicServer"
+            >
+              Use public server
+            </button>
+          </div>
+        </template>
       </form>
       <div :class="`${store.isMobile ? 'mv-half' : 'mv-1'} divider`" />
       <div class="ns title fw-500">
@@ -212,6 +223,21 @@ const handleSubmitServerAddress = () => {
 </template>
 
 <style scoped>
+.link {
+  background-color: transparent;
+  border: none;
+  color: var(--error);
+  cursor: pointer;
+  font-size: var(--spacer);
+  margin: 0;
+  outline: none;
+  padding: 0;
+  transition: color var(--transition) ease-out;
+}
+.link:hover {
+  color: var(--error-light);
+  transition: color var(--transition) ease-in;
+}
 .title {
   font-size: calc(var(--spacer) * 1.25);
 }
